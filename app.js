5bfb9a767cf3e33147f2277debf96b0c4847c33a
mariamkhalifa <m_khalifa@fanhsaweonline.ca>
2019-10-09 14:23:36 -0400

    heroku test

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req,res) => {
-    res.send('Hello World!');
-})
+// app.get('/', (req,res) => {
+//     res.send('Hello World!');
+// })
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + 'views/index.html'));
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 })
 
 app.listen(port, () => {
